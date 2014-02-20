6845e6a602454a65e15cbb1dd64da2779dd359fa
DonUrks <donurks@gmail.com>
2014-02-20 23:41:45 +0100

    Update server.js
    Added author website

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Copyright 2014 DonUrks
+// Copyright 2014 DonUrks <http://www.donurks.de/>
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
